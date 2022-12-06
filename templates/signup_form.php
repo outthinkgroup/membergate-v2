3ba9112863fad00c1a47d57be834ee416c70aa93
josh kennedy <josh@outthinkgroup.com>
2022-12-06 09:00:52 -0500

    adds templates and gitignore

--- templates/signup_form.php
+++ templates/signup_form.php
@@ -2,4 +2,8 @@
 
 ?>
 
-<div>hi</div>
+<form method="POST">
+	<input name="users_name" id="name" type="text">
+	<input name="email" id="email" type="email">
+	<button name="mg_action" value="signup">Signup</button>
+</form>
