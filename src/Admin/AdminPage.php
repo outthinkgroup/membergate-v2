d65eeccd43a04a867bd9420b4ccc15e19c968d16
josh kennedy <josh@outthinkgroup.com>
2023-01-25 11:41:24 -0500

    adds icon to admin page

--- src/Admin/AdminPage.php
+++ src/Admin/AdminPage.php
@@ -4,9 +4,11 @@ namespace Membergate\Admin;
 
 class AdminPage {
 	private $template_path;
+	private $plugin_path;
 	
-	public function __construct($template_path){
+	public function __construct($template_path, $plugin_path){
 		$this->template_path = $template_path;	
+		$this->plugin_path = $plugin_path;
 	}
 
 	public function get_page_title(){
@@ -24,6 +26,31 @@ class AdminPage {
 	public function get_slug(){
 		return "membergate-settings";
 	} 
+
+	public function get_icon_url(){
+		return "none";
+	}
+
+	public function icons_styles(){
+		echo file_get_contents($this->plugin_path . "assets/SVG/membergate_star_icon.svg");
+		?>
+			
+		<style>
+		li	.toplevel_page_membergate-settings .wp-menu-image{
+				background-color:currentColor;
+				clip-path: url(#membergate-admin-page-icon);
+				width:100%;
+			}
+
+		li	.toplevel_page_membergate-settings.wp-not-current-submenu .wp-menu-image{
+			opacity:.6
+		}
+			.toplevel_page_membergate-settings.wp-not-current-submenu:hover .wp-menu-image{
+				opacity:1;
+			}
+		</style>
+		<?php	
+	}
 
 	public function render_page(){
 		$this->render_template('admin_settings');	
--- src/Subscriber/AdminSubscriber.php
+++ src/Subscriber/AdminSubscriber.php
@@ -14,17 +14,24 @@ class AdminSubscriber implements SubscriberInterface {
 	public static function get_subscribed_events():array{
 		return [
 			'admin_menu' => 'register_admin_pages',
+			'admin_head'=> 'add_admin_icon_styles',
 		];	
 	}
 
 	public function register_admin_pages(){
-		$admin_page = new AdminPage($this->plugin_path . '/templates');	
+		$admin_page = new AdminPage($this->plugin_path . '/templates', $this->plugin_path);	
 		add_menu_page(
 			$admin_page->get_page_title(),
 			$admin_page->get_menu_title(),
 			$admin_page->get_capability(),
 			$admin_page->get_slug(),
 			[$admin_page, 'render_page'],
+			$admin_page->get_icon_url(),
 		);
+	}
+
+	public function add_admin_icon_styles(){
+		$admin_page = new AdminPage($this->plugin_path . '/templates', $this->plugin_path);	
+		$admin_page->icons_styles();
 	}
 }
