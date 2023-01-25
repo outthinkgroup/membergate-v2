d0807cf71f8cbd662e8c5c7e627d394557e45bdd
josh kennedy <josh@outthinkgroup.com>
2023-01-25 08:59:41 -0500

    all meta/option keys prefix with membergate

--- src/Settings/AccountSettings.php
+++ src/Settings/AccountSettings.php
@@ -4,7 +4,7 @@ namespace Membergate\Settings;
 
 class AccountSettings{
 	private $is_setup;
-	const WIZARD_COMPLETE_KEY = 'wizard_complete';
+	const WIZARD_COMPLETE_KEY = 'membergate_wizard_complete';
 	public function __construct(){
 		$this->is_setup = get_option(self::WIZARD_COMPLETE_KEY, false);
 	}		
