9df6a07038b526d2bd10c72a96e627552cbe0e3a
josh kennedy <josh@outthinkgroup.com>
2023-05-01 15:04:47 -0400

    set up cypress e2e

---
+++ cypress/index.d.ts
@@ -0,0 +1,14 @@
+export {}
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      // login(email: string, password: string): Chainable<void>
+      // drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
+      // dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
+      // visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
+      setMembergateCookie(): void;
+      removeMemberCookie(): void;
+			setMembergateSettings():void;
+    }
+  }
+}
---
+++ cypress/support/commands.ts
@@ -0,0 +1,40 @@
+// / <reference types="cypress" />
+// ***********************************************
+// This example commands.ts shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+//Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+//
+//
+Cypress.Commands.add("removeMemberCookie", () => {
+  cy.clearCookie("membergate_member");
+});
+
+Cypress.Commands.add("setMembergateCookie", () => {
+  cy.setCookie("membergate_member", "true");
+	cy.reload()
+});
+Cypress.Commands.add("setMembergateSettings", () => {
+  console.log(cy.exec("cypress/scripts/membergateSettings"));
+});
+
