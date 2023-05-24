99a85a7e95e80c65f29d70ef1a59c4628f2ca80c
josh kennedy <josh@outthinkgroup.com>
2023-05-24 15:37:51 -0400

    prevent error

--- src/ListProviders/EMSClients/MockClient.php
+++ src/ListProviders/EMSClients/MockClient.php
@@ -57,6 +57,7 @@ class MockClient {
     }
     public function get_tags($list_id) {
         $res = $this->get(self::URL . "/$list_id/tags");
+        debug(["GETTING TAGS",$res]);
         if (is_wp_error($res)) {
             return new PossibleError(null, $res->get_error_message());
         }
@@ -73,12 +74,18 @@ class MockClient {
     }
 
     private function get($url) {
-        return wp_remote_get($url, [
+        $res = wp_remote_get($url, [
             'headers'=>[
                 'Content-Type'=>'application/json',
                 'x-api'=>$this->api_key,
             ],
         ]);
+
+        if($res['response']['code'] !== 200){
+            $err = new \WP_Error("BAD_REQUEST",$res['response']['message']);
+            return $err;
+        }
+        return $res;
     }
     private function post($url, $body) {
         return wp_remote_post($url, [
