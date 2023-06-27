b94143443dacab7d090caacec17cbebaed992f38
josh kennedy <josh@outthinkgroup.com>
2023-06-27 16:08:29 -0400

    deps work with php 7.3

--- membergate.php
+++ membergate.php
@@ -6,15 +6,18 @@
  * Description:     PLUGIN DESCRIPTION HERE
  * Author:          YOUR NAME HERE
  * Author URI:      YOUR SITE HERE
- * Text Domain:     ot-gf-quiz
+ * Text Domain:     membergate
+ * Requires PHP:    7.3
  * Domain Path:     /languages
  * Version:         0.1.0
  */
-// this is used for asset loading for vite
+
 require __DIR__ . '/vendor/autoload.php';
 
+// this is used for asset loading for vite
 require_once dirname(__FILE__) . '/env.php';
 define('MG_IS_DEVELOPMENT', (MG_APP_ENV === 'development') || (MG_APP_ENV === 'test'));
+
 require_once 'utils.php';
 
 require_once dirname(__FILE__) . '/src/Autoloader.php';
