9c029872b688e2e9d4e907b1ba5a6e0ad7fca93a
josh kennedy <josh@outthinkgroup.com>
2024-09-24 13:06:00 -0400

    renamefile

--- assets/lib/elements/GrayBox/index.ts
+++ assets/lib/elements/GrayBox/index.ts
@@ -1,2 +1,2 @@
 export { default as GrayBox } from "./GrayBox.svelte"
-export { default as GrayBoxHeader } from "./GrayBoxHeader.svelte"
+export { default as GrayBoxHeader } from "./GrayboxHeader.svelte"
